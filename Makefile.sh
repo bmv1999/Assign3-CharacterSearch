a05cc509e464a78803c9389be38da6f71cf0acba
Benjamin Minh L Vo <47411184+bmv1999@users.noreply.github.com>
2024-02-24 06:28:21 -0800

    Create Makefile.sh

---
+++ Makefile.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+#Program: Assignment 3
+#Author: Benjamin Vo
+
+echo "Compile assign_3.c"
+gcc assign_3.c -o hw
+
+echo "Run the program: Assignment 3"
+./hw
+
+echo "The script file will terminate"
